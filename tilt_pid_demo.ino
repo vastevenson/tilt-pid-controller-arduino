6a9a38500cfdf8c85f42a304c355ed999eac1e15
vastevenson <35041906+vastevenson@users.noreply.github.com>
2022-08-14 11:18:08 -0700

    Adds plot of error, setpoint, and pid_value and cleans up code

--- tilt_pid_demo.ino
+++ tilt_pid_demo.ino
@@ -1,3 +1,10 @@
+/*
+Goal: create a simple PID controller using Arduino to manipulate the tilt to keep a ball around a set-point value.
+Also provide features to control the speed with which the servo changes angles to not damage parts.
+Date: Aug 14, 2022
+Author: Vincent Stevenson
+*/
+
 #include <Servo.h> // library for micro servo
 #include <NewPing.h> // library for the proximity sensor
 
@@ -8,7 +15,7 @@ int max_dist_cm = 25; // max distance that the proximity sensor should read out 
 
 double servo_lower_lim_deg = 80;
 double servo_upper_lim_deg = 120;
-double old_tilt_deg = servo_lower_lim_deg; // initalize the previous tilt degree
+double old_tilt_deg = servo_lower_lim_deg; // initialize the previous tilt degree
 double cumulative_error = 0; // units are cm, this is needed for I-term in PID 
 double previous_error = 0; // this is for D term in PID
 
@@ -42,7 +49,7 @@ void loop() {
 
 void update_servo(double new_tilt_deg, double old_tilt_deg, double delay_time_ms) {
   // delay time is how to control the speed the servo will change angles
-  // I added this because the servo is changing too fast
+  // I added this because the servo is moving too fast for the hot glue and paperclips
  
   // make the servo change from old to new degrees in increments, not all at once
   if (new_tilt_deg > old_tilt_deg) {
@@ -66,45 +73,25 @@ void update_servo(double new_tilt_deg, double old_tilt_deg, double delay_time_ms
 double pid(double distance_cm) {
   // input == distance from proximity sensor to ball
   // output == new angle to move the servo motor to get ball closer to setpoint
-  
-
-  
   // never let the ball get closer than 4 cm to the proximity sensor - else it isn't accurate
-  
-  
+
   double setpoint_cm = 10;
   Serial.print("setpoint_cm:");
   Serial.print(setpoint_cm);
   Serial.print(","); 
-  
 
-  
   double error = setpoint_cm - distance_cm;
   Serial.print("error_cm:");
   Serial.print(error);
   Serial.print(","); 
   
-  double min_distance_limit_cm = 4; // distance from ball to prox sensor
-  double max_distance_limit_cm = 20; 
-  
-  // these limits are for P-only!
-  double min_pid_val = (setpoint_cm - max_distance_limit_cm) * Kp; // what is the largest error possible * Kp 
-//  Serial.print("min_pid_val: ");
-//  Serial.println(min_pid_val);
-  // smallest_possible_error == ball is as far from proximity sensor as possible - reading 25 cm
-  // error == 10 - 18 = -8 cm
-  // largest_possible_error == ball is as close to proximity sensor as possible - reading 4 cm
-  // error == 10 - 4 = 6 cm
-  double max_pid_val = (setpoint_cm - min_distance_limit_cm) * Kp; // what is the smallest/most negative error possible * Kp
-//  Serial.print("max_pid_val: ");
-//  Serial.println(max_pid_val);
-  
   double p_value = error * Kp;
-//  Serial.print("p_value: ");
-//  Serial.println(p_value);
-  
+
   double i_value = cumulative_error * Ki;
+
   double d_value = (error - previous_error) * Kd;
+  // typically we would divide by the elapsed time as the D-term is checking the error rate
+  // so if the ball is moving really fast the wrong way (away from setpoint), the correction will be bigger than P-only
   
   double pid_value = p_value + i_value + d_value;
   
@@ -114,12 +101,9 @@ double pid(double distance_cm) {
   cumulative_error += error; // note that error can be + or -, this i term seeks to eliminate the offset when the P-only controller stalls
   previous_error = error; // for the next cycle, remember what this cycle's error was
   // map the pid value to a new angle for the servo to go to
-//  double new_servo_angle = pid_val_to_degree(pid_value, min_pid_val, max_pid_val, servo_lower_lim_deg, servo_upper_lim_deg);
   double servo_range = servo_upper_lim_deg - servo_lower_lim_deg;
   double new_servo_angle = map(pid_value, -1 * servo_range, servo_range, servo_lower_lim_deg, servo_upper_lim_deg);
-//  Serial.print("new_servo_angle: ");
-//  Serial.println(new_servo_angle);
-//  Serial.println();
+
   // make sure that any curveballs don't make the servo try to go to an angle outside of its operating range
   if (new_servo_angle > servo_upper_lim_deg) {
     new_servo_angle = servo_upper_lim_deg;
@@ -130,9 +114,4 @@ double pid(double distance_cm) {
   }
   
   return new_servo_angle;
-}
-// 5,0,10,0,20 -> return 10 (5-0)/(10-0) * (20-0) + 0 = 10
-double pid_val_to_degree(double pid_value, double min_pid_val, double max_pid_val, double servo_lower_lim_deg, double servo_upper_lim_deg) {
-  double ans = (pid_value - min_pid_val)/(max_pid_val - min_pid_val) * (servo_upper_lim_deg - servo_lower_lim_deg) + servo_lower_lim_deg;
-  return ans;
 }
